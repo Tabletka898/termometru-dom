9c34a710e2584d63ce232b9c5583b512408cdf5d
Tabletka898 <39377327+Tabletka898@users.noreply.github.com>
2018-05-17 17:13:08 +0300

    Add files via upload

---
+++ termometer.js
@@ -0,0 +1,21 @@
+function updateTermometer(){
+
+  // Algoritmul functiei:
+  var input = document.getElementById("temperature-input");
+  var termometer = document.getElementById("termometer-indicator");
+  var valoarea = input.value;
+  if(valoarea >= -20 &&  valoarea <= 50 ){
+    termometer.style.width =( valoarea *3 +60) +'px';
+    if(valoarea < 0){
+      termometer.style.background = "blue";
+    }
+    if(valoarea > 0){
+      termometer.style.background = "green";
+    }
+  }
+  // * sa se gaseasca campul "temperature-input"
+  // * sa se gaseasca div-ul "termometer-indicator"
+  // * sa se citeasca valoarea temperaturii din input
+  // * sa se verifice daca valoarea este cuprinsa intre -20..+50 grade C
+  // * sa se atribuie proprietatea .style.height indicatorului termometrului astfel incat fiecare grad mareste latimea cu 3px
+}
